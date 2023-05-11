9f76dfa009728ff7b946edbe3cd867cb85340ee4
Micah <micah686@github.com>
2023-05-10 21:53:57 -0700

    Updated ReactionLogic

--- Yuna/Services/Player/PlayerSearch.cs
+++ Yuna/Services/Player/PlayerSearch.cs
@@ -10,6 +10,7 @@ using Yuna.Modules;
 using System.Collections.Generic;
 using Victoria.Node;
 using Yuna.Managers;
+using Microsoft.Extensions.Primitives;
 
 namespace Yuna.Services.Player
 {
@@ -17,9 +18,8 @@ namespace Yuna.Services.Player
     {
         private static readonly Dictionary<int, string> _numberLookup = new Dictionary<int, string>()
         { {1, "one" }, {2,"two" }, {3,"three" }, {4,"four" }, {5,"five" }, {6,"six" }, {7,"seven" }, {8,"eight" }, {9,"nine" }, {10,"keycap_ten" } };
-        internal static IEmote[] EmojiList { get; private set; }
-        internal enum EmojiStates { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten }
-        internal static LavaTrack[] LavaTrackList { get; private set; }
+        internal static IEmote[] _emojiList { get; private set; }
+        internal static LavaTrack[] _lavaTrackList { get; private set; }
 
         internal static async Task<Embed> SearchAsync(LavaNode node, SocketGuildUser user, IGuild guild, IVoiceState voice, ITextChannel channel, string query)
         {
@@ -35,54 +35,30 @@ namespace Yuna.Services.Player
                 List<LavaTrack> tracks = new List<LavaTrack>();
                 int counter = 1;
 
-                try
+                foreach (var track in pls)
                 {
-                    foreach (var track in pls)
-                    {
-                        descriptionBuilder.AppendLine($"{counter}: {track.Title}");
-                        emojis.Add(Emoji.Parse($":{_numberLookup[counter]}:"));
-                        tracks.Add(track);
-                        counter++;
-                    }
-                    LavaTrackList= tracks.ToArray();
-                    EmojiList = emojis.ToArray();
+                    descriptionBuilder.AppendLine($"{counter}: {track.Title}");
+                    emojis.Add(Emoji.Parse($":{_numberLookup[counter]}:"));
+                    tracks.Add(track);
+                    counter++;
                 }
-                catch (Exception ex)
-                {
+                _lavaTrackList = tracks.ToArray();
+                _emojiList = emojis.ToArray();
 
-                    throw;
-                }
 
-                
                 return await EmbedHandler.BasicEmbed("🎶 Search", descriptionBuilder.ToString(), Color.Default);
 
             }
             return await EmbedHandler.BasicEmbed("", $"Searched ", Color.Green);
         }
-
-
-        
-        
-
-
+                
         internal static async Task AddReactions(IUserMessage message)
         {
-            try
+            foreach (var em in _emojiList)
             {
-
-                foreach (var em in EmojiList)
-                {
-                    await message.AddReactionAsync(em);
-                }
-                
-            }
-            catch (Exception ex)
-            {
-
-                throw;
+                await message.AddReactionAsync(em);
             }
         }
-
 
         public static async Task ClientReactionAdded(Cacheable<IUserMessage, ulong> message, Cacheable<IMessageChannel, ulong> messageChannel, SocketReaction reaction, LavaNode node)
         {
@@ -96,7 +72,7 @@ namespace Yuna.Services.Player
                 }
                 var msg = await message.GetOrDownloadAsync();
 
-                if (!EmojiList.Contains(reaction.Emote))
+                if (!_emojiList.Contains(reaction.Emote))
                 {
                     await Task.CompletedTask;
                     return;
@@ -104,14 +80,22 @@ namespace Yuna.Services.Player
 
                 _ = Task.Run(async () =>
                 {
-                    EmojiStates currentState = (EmojiStates)Array.IndexOf(EmojiList, reaction.Emote);
+                    var currentState = Array.IndexOf(_emojiList, reaction.Emote);
 
                     if (reaction.UserId == ConfigManager.BotConfig.BotUserId)
                     {
                         await Task.CompletedTask;
                         return;
                     }
-                    await msg.RemoveReactionAsync(reaction.Emote, reaction.User.Value, options: new RequestOptions { RetryMode = RetryMode.RetryRatelimit });
+                    try
+                    {
+                        await msg.RemoveReactionAsync(reaction.Emote, reaction.User.Value, options: new RequestOptions { RetryMode = RetryMode.RetryRatelimit });
+                    }
+                    catch (Exception)
+                    {
+                        return;                       
+                    }
+                    
 
                     try
                     {
@@ -128,52 +112,20 @@ namespace Yuna.Services.Player
 
                     node.TryGetPlayer(guildChannel.Guild, out var player);
 
-                    if (!(player.PlayerState is PlayerState.Playing or PlayerState.Paused))
+
+                    
+                    if(currentState >= 0 && currentState <= 9)
                     {
-                        return;
+                        await player.PlayAsync(_lavaTrackList[currentState - 1]);
                     }
 
-                    switch (currentState)
-                    {
-                        case EmojiStates.One:
-                            await player.PlayAsync(LavaTrackList[0]);
-                            break;
-                        case EmojiStates.Two:
-                            await player.PlayAsync(LavaTrackList[1]);
-                            break;
-                        case EmojiStates.Three:
-                            await player.PlayAsync(LavaTrackList[2]);
-                            break;
-                        case EmojiStates.Four:
-                            await player.PlayAsync(LavaTrackList[3]);
-                            break;
-                        case EmojiStates.Five:
-                            await player.PlayAsync(LavaTrackList[4]);
-                            break;
-                        case EmojiStates.Six:
-                            await player.PlayAsync(LavaTrackList[5]);
-                            break;
-                        case EmojiStates.Seven:
-                            await player.PlayAsync(LavaTrackList[6]);
-                            break;
-                        case EmojiStates.Eight:
-                            await player.PlayAsync(LavaTrackList[7]);
-                            break;
-                        case EmojiStates.Nine:
-                            await player.PlayAsync(LavaTrackList[8]);
-                            break;
-                        case EmojiStates.Ten:
-                            await player.PlayAsync(LavaTrackList[9]);
-                            break;
-                        default:
-                            break;
-                    }
+                    
                 });
             }
-            catch (Exception ex)
+            catch (Exception)
             {
 
-                throw;
+                return;
             }
 
         }
